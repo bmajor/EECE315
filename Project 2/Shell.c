5fc1e78cfabf26575f6bd8850eb46642a77f111d
Leslie Mui <leslie_mui@telus.net>
2013-02-24 17:18:18 -0800

    Modified to work with Linux rather than windows.

--- Project 2/Shell.c
+++ Project 2/Shell.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Windows.h>
 #include <string.h>
-#include <sys/utsname.h> // include this to use uname();
+#include <sys/param.h>
+#include <unistd.h>
 
 struct command_t { 
 	char *name;
@@ -10,42 +10,31 @@ struct command_t {
 	char *argv[];
 };
 
-struct utsname unameData;
 
 int main () {
 
 	struct command_t *command; // Shell initialization
-	wchar_t CompName [MAX_COMPUTERNAME_LENGTH + 1];
-	DWORD CNameSize = sizeof ( CompName);
-	GetComputerName( &CompName[0], &CNameSize);
 	
-	// Or could try this; to get comp name;
-	if ( uname(&unameData) == 0) {
-//	printf("%s\n%s\n%s\n%s\n%s\n", unameData.sysname, unameData.nodename, unameData.release, unameData.version, unameData.machine);
-	int i = 0;
-	for (i; i < 8; i++) // arbitrary size
-		if (  unameData.nodename[i] != '\0')
-			printf("unameData.nodename[i] = %c \n", unameData.nodename[i]);
-	} else
-		printf("uname() failure\n");
-	}
+	char CompName[MAXHOSTNAMELEN+1];
+	size_t CompNameLen = MAXHOSTNAMELEN;
+	gethostname(CompName, CompNameLen);
+
 	
-	while(1)
+	//while(1)
 	{
 		int i;
-		for(i=0;i<MAX_COMPUTERNAME_LENGTH-1;i++){
-			printf("%c", CompName[i]);
+		for(i=0;CompName[i] != '\0';i++){		
+		printf("%c",CompName[i]);
 		}
-		printf("$");
-
-
+		printf("$~$");
 
 		// Print the prompt string
 		// Read the command line and parse it 
 		// Find the full pathname for the file
 		// Create a process to execute the command
 		// Parent waits until child finishes executing command }
-		system ("Pause");
+		printf("\nPress Enter to exit");
+		while(getchar() != '\n');
 	}
 
 	return 0;
