1c4ba6ead69ba263f953f4f05bcbe69c41ec8fb8
Sahli <pcd_bdr@yahoo.com>
2013-02-26 18:11:17 -0800

    made the getPathName method and changeDir method

--- Project 2/Shell.c
+++ Project 2/Shell.c
@@ -11,42 +11,49 @@ struct command_t {
 	char *argv[];
 };
 
+void getPathName() {
+	char PathName [PATH_MAX+1];	
+	getcwd(PathName, PATH_MAX);
+
+	if (getcwd(PathName, PATH_MAX) == NULL){
+			perror("getcwd() error");
+		}
+		else {	
+			printf("%s$ ",PathName); // Print full pathname + $
+		}
+}
+
+void changeDir(const char *newDir) {
+	int ret; // if fail to change dir, ret should eq to -1 but the current working dir shall remain unchanged
+	ret = chdir(newDir);
+	getPathName();
+}
 
 int main () {
 
 	struct command_t *command = malloc(sizeof *command); // Shell initialization
-	
+
 	char CompName[MAXHOSTNAMELEN+1];
 	gethostname(CompName, MAXHOSTNAMELEN);
-	
-	char PathName [PATH_MAX+1];	
-	getcwd(PathName, PATH_MAX);
-	
+
 	char CommandLine[PATH_MAX*2];
-	
+
 	//while(1) //Continuously run the command line UNLESS exit is called.
 	{
-		
 		int k=0;
 		//Print the prompt String
 		printf("%s:~",CompName);
-		if (getcwd(PathName, PATH_MAX) == NULL){
-			perror("getcwd() error");
-		}
-		else {	
-			printf("%s$ ",PathName);
-		}
+		getPathName();
 
 		// Read the command line and parse it
 		char* Args;
-		
+
 		gets(CommandLine);
 		Args = strtok (CommandLine, " ");
 		command->name = malloc(NAME_MAX+1);
 		command->name = Args;
-		printf("%s", command->name);
-		
-		
+		printf("%s\n", command->name);
+
 		while(Args != NULL){
 			command->argv[k] = malloc(PATH_MAX);
 			strcpy(command->argv[k], Args);
@@ -54,10 +61,14 @@ int main () {
 			Args = strtok (NULL, " ");
 			k++;		
 		}
-		
+
+		// Change the directory if the first argument is cd
+		if ( strcmp(command->argv[0],"cd") == 0 )
+			changeDir(command->argv[1]);
+
 		// Find the full pathname for the file
-		
-		
+
+
 		// Create a process to execute the command
 		// Parent waits until child finishes executing command }
 		printf("\nPress Enter to exit\n");
