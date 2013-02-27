0a95cd3d312a468293e19b7669b820d5eba172b5
Leslie Mui <leslie_mui@telus.net>
2013-02-26 20:05:54 -0800

    Implemented path finder

--- Project 2/Shell.c
+++ Project 2/Shell.c
@@ -12,22 +12,9 @@ struct command_t {
 	char *argv[];
 };
 
-void getPathName() {
-	char PathName [PATH_MAX+1];	
-	getcwd(PathName, PATH_MAX);
-
-	if (getcwd(PathName, PATH_MAX) == NULL){
-			perror("getcwd() error");
-		}
-		else {	
-			printf("%s$ ",PathName); // Print full pathname + $
-		}
-}
-
 void changeDir(const char *newDir) {
 	int ret; // if fail to change dir, ret should eq to -1 but the current working dir shall remain unchanged
 	ret = chdir(newDir);
-	getPathName();
 }
 
 int main () {
@@ -71,11 +58,8 @@ int main () {
 			strcpy(shortPath, Args);
 			strcpy(command->name, last+1);
 			Args = command->name;
-			realpath(shortPath, Path);
-			printf("%s", Path);
 		}
-		
-		
+				
 		while(Args != NULL){
 			command->argv[k] = malloc(PATH_MAX);
 			strcpy(command->argv[k], Args);
@@ -86,22 +70,30 @@ int main () {
 		command -> argv[k] = NULL;
 		
 		// Find the full pathname for the file
-		// Change the directory if the first argument is cd
-		if ( strcmp(command->argv[0],"cd") == 0 )
-			changeDir(command->argv[1]);
 		
-		// Create a process to execute the command
-		/*
-		pid_t childPID;		
-		childPID = fork();
-		if(childPID >= 0){
-			execv(path, command.argv);
+		realpath(shortPath,Path);
+		
+		// Change the directory if the first argument is cd
+		if (strcmp(command->argv[0],"cd") == 0 ){
+			changeDir(command->argv[1]);
 		}
+		else if (strcmp(command->argv[0],"[something]") == 0 ){
+		
+		}
+		
 		else{
-		printf("\nCould not fork process");
-		}
-		*/
-		// Parent waits until child finishes executing command }
+			// Create a process to execute the command
+			/*	
+			pid_t childPID;		
+			childPID = fork();
+			if(childPID >= 0){
+				execv(path, command.argv);
+			}
+			else{
+			printf("\nCould not fork process");
+			}
+			*/
+			// Parent waits until child finishes executing command }
 		printf("\nPress Enter to exit\n");
 		while(getchar() != '\n');
 	}	
