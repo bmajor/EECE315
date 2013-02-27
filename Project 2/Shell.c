e8a6477b086369f82dbd8e9c037c4c494b5d5508
Leslie Mui <leslie_mui@telus.net>
2013-02-26 21:28:51 -0800

    proper cdprintenvexit.

--- Project 2/Shell.c
+++ Project 2/Shell.c
@@ -12,26 +12,21 @@ struct command_t {
 	char *argv[];
 };
 
-void changeDir(const char *newDir) {
-	int ret; // if fail to change dir, ret should eq to -1 but the current working dir shall remain unchanged
-	ret = chdir(newDir);
-}
-
 int main () {
 
 	struct command_t *command = malloc(sizeof *command); // Shell initialization
-
+	
 	char CompName[MAXHOSTNAMELEN+1];
 	gethostname(CompName, MAXHOSTNAMELEN);
-
+	
 	char PathName [PATH_MAX+1];	
 	getcwd(PathName, PATH_MAX);
-
+	
 	char CommandLine[PATH_MAX*2];
-
-	//while(1) //Continuously run the command line UNLESS exit is called.
+	
+	while(1) //Continuously run the command line UNLESS exit is called.
 	{
-
+		
 		int k=0;
 		//Print the prompt String
 		printf("%s:~",CompName);
@@ -43,13 +38,14 @@ int main () {
 		}
 
 		// Read the command line and parse it
-		char* Args;
+		char* Args = malloc(PATH_MAX);
 		char shortPath [PATH_MAX];
 		char Path [PATH_MAX];
-
+		
 		gets(CommandLine);
 		Args = strtok (CommandLine, " ");
 		command->name = malloc(NAME_MAX+1);
+		
 		if(strrchr(Args, '/') == NULL){			
 			command->name = Args;
 		}
@@ -59,7 +55,7 @@ int main () {
 			strcpy(command->name, last+1);
 			Args = command->name;
 		}
-
+				
 		while(Args != NULL){
 			command->argv[k] = malloc(PATH_MAX);
 			strcpy(command->argv[k], Args);
@@ -67,38 +63,55 @@ int main () {
 			Args = strtok (NULL, " ");
 			k++;		
 		}
-		command -> argv[k] = NULL;
-
-		// Find the full pathname for the file
-
-		realpath(shortPath,Path);
-
-		// Change the directory if the first argument is cd
-		if (strcmp(command->argv[0],"cd") == 0 ){
-			if ( command->argc < 2 ) {
-				changeDir(getenv("HOME"));
-			} else {
-				changeDir(command->argv[1]);
-			}
-		} else if (strcmp(command->argv[0],"[something]") == 0 ){
 		
-		} else {
-			// Create a process to execute the command
-			/*	
-			pid_t childPID;		
-			childPID = fork();
-			if(childPID >= 0){
-				execv(path, command.argv);
+		command -> argv[k] = NULL;
+		
+		if (strcmp(command->argv[0],"exit") == 0 ){
+			return 0;
+		}
+		
+		pid_t childPID;
+		childPID = fork();
+		//Fork a child here and run all following instructions as child
+		if(childPID == 0){
+				
+			// Change the directory if the first argument is cd
+			if (strcmp(command->argv[0],"cd") == 0 ){
+
+				if(command->argc <2) {
+					chdir(getenv("HOME"));
+				}
+				else{
+					chdir(command->argv[1]);
+				}
 			}
+			
+			else if (strcmp(command->argv[0],"exit") == 0 ){
+				return 0;
+			}
+			
+			else if (strcmp(command->argv[0],"printenv") == 0 ){
+				printf("%s", getenv("PATH"));
+			}
+			
 			else{
-			printf("\nCould not fork process");
+			//	realpath(shortPath,Path);
+			//	execv(Path, command.argv);
 			}
-			*/
-			// Parent waits until child finishes executing command }
+			return 0;
+		}
+		else if (childPID == -1){
+			printf("\nCould not fork process");
+		}
+		
+		// Parent waits until child finishes executing command }
+		
+		else{
+			wait();
+		}	
+				
+		
 	}	
-	printf("\nPress Enter to exit\n");
-	while(getchar() != '\n');
 
 	return 0;
-	}
 }
